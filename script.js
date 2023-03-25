c5c8ebe3e38fb443b17bf3cb1a45eb21c6269735
Komal Meena <21meenakk@gmail.com>
2023-03-25 23:05:56 +0530

    msg

--- script.js
+++ script.js
@@ -1,47 +1,61 @@
-// const showMapbtn = document.getElementById('getLocationBtn');
-// const removeBtn = document.getElementById('removeLocationBtn');
-// const x = document.getElementById('map');
+const mapDiv = document.getElementById('map');
+const getLocationBtn = document.getElementById('getLocationBtn');
+const removeLocationBtn = document.getElementById('removeLocationBtn');
 
 
+const latitude = localStorage.getItem('lat');
+const longitude = localStorage.getItem('long');
 
-let btn = document.querySelector('#getLocationBtn');
-let x = document.getElementById('map');
-let removebtn = document.querySelector('#removeLocationBtn');
-let pos = false;
+
+if (latitude && longitude) {
+  getLocationBtn.disabled = true;
+  displayMap(latitude, longitude);
+}
+
 function getLocation() {
     if (navigator.geolocation) {
-        if (localStorage.getItem('lat') || localStorage.getItem('long')) {
-            pos = false;
-            showPosition(pos);
-        }
-        else {
-            pos = true;
-            navigator.geolocation.getCurrentPosition(showPosition);
-
-        }
-
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        // alert('Geolocation is not supported by this browser.');
+        mapDiv.innerText="Geolocation is not supported by this browser."
     }
-}
-
+  }
+// callback function for getCurrentPosition
 function showPosition(position) {
-    if (!pos) {
-        x.innerHTML = "<iframe src='https://maps.google.com/maps?q=" + localStorage.getItem('lat') + "," + localStorage.getItem('long') + "&z=15&output=embed' width='80%' height='600' frameborder='0' style='border:0'></iframe>";
-    }
-    else {
-        localStorage.setItem('lat', position.coords.latitude);
-        localStorage.setItem('long', position.coords.longitude);
-        x.innerHTML = "<iframe src='https://maps.google.com/maps?q=" + position.coords.latitude + "," + position.coords.longitude + "&z=15&output=embed' width='80%' height='500' frameborder='0' style='border:0'></iframe>";
-    }
-}
-btn.addEventListener('click', getLocation);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+   
+    localStorage.setItem('lat', latitude);
+    localStorage.setItem('long', longitude);
+    
+    getLocationBtn.disabled = true;
+    displayMap(latitude, longitude);
+  }
 
-function removelocation() {
-    if (localStorage.getItem('lat') || localStorage.getItem('long')) {
-        localStorage.clear();
-    } else {
-        alert('No location in Local Storage');
-    }
+  getLocationBtn.addEventListener('click', getLocation);
+
+// add event listener to removeLocation button
+removeLocationBtn.addEventListener('click', () => {
+  // remove latitude and longitude from local storage
+  localStorage.removeItem('lat');
+  localStorage.removeItem('long');
+  // enable getLocation button and remove map
+  getLocationBtn.disabled = false;
+  mapDiv.innerHTML = '';
+});
+
+
+
+// function to display map using Google Maps
+function displayMap(latitude, longitude) {
+
+  const mapUrl = `https://maps.google.com/maps?q=${latitude},${longitude}&output=embed`;
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute('src', mapUrl);
+  iframe.setAttribute('width', '100%');
+  iframe.setAttribute('height', '500');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('style', 'border:0');
+  
+  mapDiv.appendChild(iframe);
 }
-removebtn.addEventListener('click', removelocation);
